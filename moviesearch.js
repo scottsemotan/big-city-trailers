898b1164cdffbf1a230938ec81c8c9bff93098b4
Scott Semotan <semotan.scott@gmail.com>
2021-03-10 11:07:20 -0500

    updated api

--- moviesearch.js
+++ moviesearch.js
@@ -32,7 +32,7 @@ var granimInstance = new Granim({
 var movieBody = document.getElementById('movieBody');
 var submitButton = document.getElementById('submitButton')
 var oMDB = 'c366972e'
-var tasteDive = '380370-BigCityT-ZF132C2Q'
+var tasteDive = '380370-BigCityT-H919N0I1'
 var cards = document.getElementById("cards")
 
 
